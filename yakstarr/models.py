4f65fd5452e2827a7a8d83e1e181fa751bf54138
Novrule <2000dnjswns@naver.com>
2024-06-23 16:41:53 +0900

    feat : DB 구성 변경

--- yakstarr/models.py
+++ yakstarr/models.py
@@ -5,12 +5,16 @@ class DetailedDrug(models.Model):
     drug_image = models.ImageField(blank=True, upload_to='drug_images/')
     drug_name = models.CharField(max_length=255, blank=False)
     drug_summary = models.CharField(max_length=255, blank=True, null=True)
-    drug_description = models.TextField(blank=True, null=True)
+    drug_element = models.CharField(max_length=255, blank=True, null=True)
+    drug_company = models.CharField(max_length=255, blank=True, null=True)
+    drug_effect = models.CharField(max_length=255, blank=True, null=True)
+    drug_recipe = models.CharField(max_length=255, blank=True, null=True)
+    drug_method = models.CharField(max_length=255, blank=True, null=True)
+    drug_warning = models.CharField(max_length=255, blank=True, null=True)
     drug_category = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.drug_name
-
 
 class DrugReview(models.Model):
     drug = models.ForeignKey(DetailedDrug, on_delete=models.CASCADE)
--- yakstarr/serializers.py
+++ yakstarr/serializers.py
@@ -1,10 +1,20 @@
 from rest_framework import serializers
 from .models import DetailedDrug, DrugReview
+from django.db.models import Avg
 
 class DetailedDrugSerializer(serializers.ModelSerializer):
+    review_count = serializers.SerializerMethodField()
+    average_rating = serializers.SerializerMethodField()
+
     class Meta:
         model = DetailedDrug
         fields = '__all__'
+
+    def get_review_count(self, obj):
+        return DrugReview.objects.filter(drug=obj).count()
+
+    def get_average_rating(self, obj):
+        return DrugReview.objects.filter(drug=obj).aggregate(Avg('rating'))['rating__avg']
 
 class DrugReviewSerializer(serializers.ModelSerializer):
     class Meta:
